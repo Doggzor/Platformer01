40085529d724dfb295fd21f2278b6adc6b3cf71a
Luka Bućan <81297996+Doggzor@users.noreply.github.com>
2022-01-20 13:47:00 +0100

    Improved Jump + Added Flip
    Fixed jump inconsistencies and added a method to flip the sprite according to the facing direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,15 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private new BoxCollider2D collider;
     private LayerMask groundLayer;
+    private GameObject sprite;
+
     private float gravityScaleNormal = 0f;
     private float gravityScaleFalling = 0f;
+
     private float dirX = 0f;
+
     private float jumpPressTime = -1f;
-    private float timeLastGrounded = -1f;
+    private float timeLastGrounded = -1f;   
 
     enum State
     {
@@ -34,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
+        sprite = transform.Find("Sprite").gameObject;
         groundLayer = LayerMask.GetMask("Ground");
         gravityScaleNormal = rb.gravityScale;
         gravityScaleFalling = rb.gravityScale * gravityFallMultiplier;
@@ -47,29 +53,31 @@ public class PlayerMovement : MonoBehaviour
     {
         State debugState = state;
 
-        dirX = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.UpArrow)) jumpPressTime = Time.time;
-        if (IsGrounded()) timeLastGrounded = Time.time;
+        HandleInput();
+        if (HasFacingDirectionChanged())
+            Flip();
+        if (IsGrounded())
+            timeLastGrounded = Time.time;
 
-        DetectState();
+        state = DetectState();
         HandleGravity();
 
         if (state != debugState)
         {
             Debug.Log(state.ToString());
             if (state == State.FALLING)
-                Debug.Log($"Height reached: {gameObject.transform.position.y}");
+                Debug.Log($"Height reached: {transform.position.y}");
         }
     }
 
-    private void HandleGravity()
+    private void Flip()
     {
-        rb.gravityScale =
-            state == State.FALLING ? gravityScaleFalling :
-            rb.gravityScale > gravityScaleFalling ? rb.gravityScale : //Prevent resetting the gravity mid-air
-            gravityScaleNormal;
-        if (!Input.GetKey(KeyCode.UpArrow) && state == State.JUMPING)
-            rb.gravityScale = gravityScaleFalling * 1.66f;
+        sprite.transform.localScale *= new Vector2(-1, 1);
+    }
+
+    private bool HasFacingDirectionChanged()
+    {
+        return Mathf.Abs(sprite.transform.localScale.x - dirX) > 1.1f;
     }
 
     private void FixedUpdate()
@@ -79,15 +87,45 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(Mathf.Min(Mathf.Abs(rb.velocity.x), speed) * dirX, rb.velocity.y);
 
         //Jumping
-        if (Time.time - jumpPressTime <= jumpToleranceTime && Time.time - timeLastGrounded <= coyoteTime) Jump();
+        if (Time.time - jumpPressTime <= jumpToleranceTime &&
+            Time.time - timeLastGrounded <= coyoteTime)
+            Jump();
 
         
     }
 
+    private void HandleInput()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.UpArrow)) jumpPressTime = Time.time;
+    }
+
+    private void HandleGravity()
+    {
+        rb.gravityScale = state switch
+        {
+            State.FALLING => gravityScaleFalling,
+            State.JUMPING => Input.GetKeyUp(KeyCode.UpArrow) ? gravityScaleFalling * 1.66f : rb.gravityScale,
+            _ => gravityScaleNormal
+        };
+
+        //OLD CODE
+        /*
+                rb.gravityScale =
+                  state == State.FALLING ? gravityScaleFalling :
+                  rb.gravityScale > gravityScaleFalling ? rb.gravityScale : //Prevent resetting the gravity mid-air
+                  gravityScaleNormal;
+                if (!Input.GetKey(KeyCode.UpArrow) && state == State.JUMPING)
+                    rb.gravityScale = gravityScaleFalling * 1.66f;
+        */
+    }
     private void Jump()
     {
+        //Reset some values before performing the actual jump
+        jumpPressTime = -1f; //Important for consistent jump heights
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.gravityScale = gravityScaleNormal;
+        //Actual jump
         rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 
@@ -96,16 +134,15 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.BoxCast(collider.bounds.center, collider.size, 0f, Vector2.down, 0.2f, groundLayer);
     }
 
-    private void DetectState()
+    private State DetectState()
     {
         if (IsGrounded())
         {
-            state = Mathf.Abs(dirX) > 0.05f ? State.RUNNING : State.IDLE;
-            return;
+            return Mathf.Abs(dirX) > 0.05f ? State.RUNNING : State.IDLE;
         }
-        state = rb.velocity.y > 0.05f ? State.JUMPING : State.FALLING;
+        return rb.velocity.y > 0.05f ? State.JUMPING : State.FALLING;
     }
-    private void HandleState()
+    private void HandleAnimations()
     {
         switch (state)
         {
